d78a76120935cabc1f8502f8d5060055522f55cc
MakeTSGreatAgain <andrii.mus00@gmail.com>
2022-02-16 09:31:23 +0200

    my first basis step

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component, useState } from 'react';
+import Header from './components/header/Header.jsx';
+import Calendar from './components/calendar/Calendar.jsx';
+
+import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
+
+import './common.scss';
+
+const App = () => {
+  const [weekStartDate, setWeekStarDate] = useState(new Date())
+  const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+  return (
+    <>
+      <Header />
+      <Calendar weekDates={weekDates} />
+    </>
+  );
+}
+
+
+
+
+export default App;
---
+++ src/components/calendar/Calendar.jsx
@@ -0,0 +1,29 @@
+import React, { Component, useState } from 'react';
+
+import Navigation from './../navigation/Navigation';
+import Week from '../week/Week';
+import Sidebar from '../sidebar/Sidebar';
+import arrEvents from '../../gateway/events';
+
+import './calendar.scss';
+
+
+const Calendar = ({ weekDates }) => {
+  const [events, setEvents] = useState(arrEvents);
+
+  return (
+    <section className="calendar">
+      <Navigation weekDates={weekDates} />
+      <div className="calendar__body">
+        <div className="calendar__week-container">
+          <Sidebar />
+          <Week weekDates={weekDates} events={events} />
+        </div>
+      </div>
+    </section>)
+}
+
+
+
+
+export default Calendar;
